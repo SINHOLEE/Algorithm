aa2264adc1833abd1bd834507ee05a35ec6ccab3
이신호 <dltlsgh5@naver.com>
2019-11-06 01:12:11 +0900

    백준  최단경로문제 다익스트라 알고리즘 적용 풀이 성공

--- 9월/서울2반9월16일/순열.py
+++ 9월/서울2반9월16일/순열.py
@@ -1,20 +1,32 @@
-arr = [3, 1, 6, 4]
+# arr = [3, 1, 6, 4]
+#
+# def perm(r):
+#     global count
+#     count+= 1
+#     if len(arr) == r:
+#         print(temp, 'count = ',count)
+#         return
+#     for j in range(len(arr)):
+#         if visited[j] == False:
+#             visited[j] = True
+#             temp[r] = arr[j]
+#             perm(r + 1)
+#             visited[j] = False
+#
+#
+# visited = [False] * len(arr)
+# temp = [0] * len(arr)
+# count = 0
+# perm(0)
 
-def perm(r):
-    global count
-    count+= 1
-    if len(arr) == r:
-        print(temp, 'count = ',count)
+def perm(depth, temp):
+    if depth == 3:
+        print(temp)
         return
-    for j in range(len(arr)):
-        if visited[j] == False:
-            visited[j] = True
-            temp[r] = arr[j]
-            perm(r + 1)
-            visited[j] = False
-
-
-visited = [False] * len(arr)
-temp = [0] * len(arr)
-count = 0
-perm(0)
+    for i in range(3):
+        # if i == 1:
+        #     continue
+        temp[depth], temp[i] = temp[i], temp[depth]
+        perm(depth+1, temp)
+        temp[depth], temp[i] = temp[i], temp[depth]
+perm(0, [0,1,2])
