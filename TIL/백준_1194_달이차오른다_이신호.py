d48c28a1a184aeaa17fa20ae65167dd35b223e22
이신호 <dltlsgh5@naver.com>
2019-12-19 15:36:40 +0900

    백준1194 달이차오른다 using by bitmasking

---
+++ TIL/백준_1194_달이차오른다_이신호.py
@@ -0,0 +1,47 @@
+from collections import deque
+n, m = map(int, input().split())
+mat = [input() for _ in range(n)]
+
+di = [0, 0, -1, 1]
+dj = [-1, 1, 0, 0]
+
+for i in range(n):
+    for j in range(m):
+        if mat[i][j] == '0':
+            start = (i, j)
+            mat[i] = mat[i].replace('0', '.')
+
+keys = [[[0]*(2**6) for i in range(m)] for _ in range(n)]
+
+dq = deque([(0, 0, start[0], start[1])])
+keys[start[0]][start[1]][0] = 1
+
+flag = True
+while dq:
+    cnt, key, x, y = dq.popleft()
+
+    if mat[x][y] == '1':
+        flag = False
+        break
+    temp = key
+    for k in range(4):
+        newX, newY = x+di[k], y+dj[k]
+        if 0 <= newX < n and 0 <= newY < m:
+            if keys[newX][newY][key]:
+                continue
+            if mat[newX][newY] != '#':
+                if mat[newX][newY] in ('a', 'b', 'c', 'd', 'e', 'f'):
+                    if key & 1 << (ord(mat[newX][newY]) - ord('a')) == 0:
+                        temp = key
+                        key = key | 1 << (ord(mat[newX][newY]) - ord('a'))
+
+                elif mat[newX][newY] in ('A', 'B', 'C', 'D', 'E', 'F'):
+                    if key & 1 << (ord(mat[newX][newY]) - ord('A')) == 0:
+                        continue
+                keys[newX][newY][key] = 1
+                dq.append((cnt+1, key, newX, newY))
+                key = temp
+if flag:
+    print(-1)
+else:
+    print(cnt)
--- TIL/백준_4195_친구네트워크_이신호.py
+++ TIL/백준_4195_친구네트워크_이신호.py
@@ -1,36 +1,38 @@
 import sys
 input = sys.stdin.readline
-def find(x):
-    if root[x] == x:
-        return x
-    y = find(root[x])
-    root[x] = y
 
+
+def find(x):
+    if parent[x] < 0:
+        return x
+    y = find(parent[x])
+    parent[x] = y
     return y
 
-def union(x,y):
+
+def union(x, y):
     x = find(x)
     y = find(y)
-    if root[y] == x:
-        return
-    root[y] = x
-    network[x] = network[x] + network[y]
+    if y == x:
+        return -1 * parent[x]
+    parent[x] += parent[y]
+    parent[y] = x
+    return -1 * parent[x]
+
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     name = {}
     cnt = 0
-    root = []
-    network = []
+    parent = []
     for _ in range(n):
         temp = list(map(str, input().strip().split()))
         for a in temp:
-            if name.get(a) == None:
+            if name.get(a) is None:
                 name[a] = cnt
-                root.append(cnt)
-                network.append(1)
+                parent.append(-1)
                 cnt += 1
 
-        union(name[temp[0]], name[temp[1]])
-        print(network[find(name[temp[0]])])
+        print(union(name[temp[0]], name[temp[1]]))
+
