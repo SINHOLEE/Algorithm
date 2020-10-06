9a0df1bb8a5886cd0b9de3acf171be013404b280
sinho <dltlsgh5@gmail.com>
2020-10-07 08:39:31 +0900

    cpp 스타일로 리팩토링

--- python/seq_arr_map.py
+++ python/seq_arr_map.py
@@ -3,52 +3,65 @@ class Person:
         self.m_id = m_id
         self.name = name
     
-    def set_idx(self, idx):
-        self.idx = idx
-    
     def __repr__(self):
-        return "아이디: %s, 이름: %s, 인덱스: %s" % (self.m_id, self.name, self.idx)
+        return "아이디: %s, 이름: %s" % (self.m_id, self.name)
 
 class Map:
-
     def __init__(self):
-        self.arr = []
+        self.arr = [None] * 10
         self.m_len = 0
+        self.size = 10
 
     def insert(self, p):
-        p.set_idx(self.m_len)
-        self.arr.append(p)
-        self.m_len += 1
-        return True
+        if self.size > self.m_len:
+            self.arr[self.m_len] = p
+            self.m_len += 1
+            return True
+        else:
+            return False
     
     def find(self, key):
         target_idx = self._find_idx(key)
         return self.arr[target_idx]
         
     def _find_idx(self, key):
-        for p in self.arr:
-            if p.m_id == key:
-                return p.idx
+        for i in range(self.m_len):
+            if self.arr[i].m_id == key:
+                return i
         return None
 
     def remove(self, key):
         target_idx = self._find_idx(key)
         if target_idx is not None:
-            self.arr.pop(target_idx)  # O(n) 
+            return self.remove_at(target_idx) 
         else:
-            print("삭제불가")
+            print("key: %s 삭제불가" % key)
+
+    def remove_at(self, target_idx):
+        for i in range(target_idx+1, self.m_len):
+            self.arr[i-1] = self.arr[i]
+        self.m_len -=1
+        return True
 
     def print_list(self):
-        for p in self.arr:
-            print(p.m_id, p.name)
+        print("self.m_len: %s " % self.m_len)
+        print(self.arr)
+        for i in range(self.m_len):
+            print(self.arr[i])
 
     
 
 m = Map()
 
 m.insert(Person(1, "sinho"))
-m.insert(Person(2, "hh"))
-m.print_list()
+m.insert(Person(2, "a"))
+m.insert(Person(4, "b"))
+m.insert(Person(7, "c"))
+m.insert(Person(3, "d"))
+# m.print_list()
 
-m.remove(1)
+m.remove(6)
+m.remove(7)
+m.insert(Person(10, "10"))
+m.insert(Person(10, "a"))
 m.print_list()
